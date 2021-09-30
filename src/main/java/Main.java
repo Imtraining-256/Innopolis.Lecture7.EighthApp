3d693f688e68af1b90dae873dcde0ca98fb579cb
atenyaeves <atenyaev@protonmail.com>
2021-09-30 22:00:06 +0700

    Threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    System.out.println("5 seconds");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
+        Thread thread2 = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(7000);
+                    System.out.println("7 seconds");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread2.start();
+        thread1.start();
+        new Thread(new TenSecondsWaiter()).start();
+        System.out.println("Main Threed");
     }
 }
